0924e4514cad5fb18cfd4252a42ea34207bea333
Iramar Falcão <iramar.j@hotmail.com>
2022-04-23 16:45:18 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chat/ui/screens/SignIn/sign_in.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignIn(),
+    );
+  }
+}
---
+++ lib/ui/screens/SignIn/sign_in.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Sign In',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 32.0,
+              fontWeight: FontWeight.bold,
+            )),
+        elevation: 0.0,
+        centerTitle: false,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 64.0),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 8.0,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 8.0,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          RaisedButton(
+            onPressed: () {},
+            child: const Text('Sign In'),
+          ),
+          const SizedBox(height: 16.0),
+          RaisedButton(
+            onPressed: () {},
+            child: const Text('Sign Up'),
+          ),
+        ],
+      ),
+    );
+  }
+}
